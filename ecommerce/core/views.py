f5f48e75b14ebe778d59d639d322a6188b75704b
gitsubash44 <dhamisubash866@gmail.com>
2024-11-05 08:27:03 +0545

    adding

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -66,3 +66,37 @@ def add_to_cart(request,pk):
         order.items.add(order_item)
         messages.info(request,"Item Added To Cart")
         return  redirect("product_desc",pk=pk) 
+    
+    
+def add_item(request,pk):
+    # get that particular product of id = pk
+    product = Product.objects.get(pk=pk)
+    # create order item 
+    order_item, created  = OrderItem.objects.get_or_create(
+        product = product,
+        user = request.user,
+        ordered = False,
+    )
+    
+    # Get query set of order object of particular user
+    order_qs = Order.objects.filter(user=request.user,ordered=False)
+    if order_qs.exists(): 
+        order = order_qs[0]
+        if order.items.filter(product__pk = pk).exists():
+            if order_item.quantity < product.product_available_count:
+                order_item.quantity +=1
+                order_item.save()
+                messages.info(request, "Added quantity Item. ")
+                return redirect("orderlist")
+            else:
+                messages.info(request,"Sorry!, Product is out of stock")
+        else:
+            order_item.add(order_item,)
+            messages.info(request,"Item added to cart")   
+            return redirect("product_desc",pk=pk)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(user=request.user,ordered_date = ordered_date)
+        order.items.add(order_item)
+        messages.info(request,"Item Added To Cart")
+        return  redirect("product_desc",pk=pk) 
