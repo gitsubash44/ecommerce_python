86c9dcb31cbfe4185c631f87bcc89cab205074ff
gitsubash44 <dhamisubash866@gmail.com>
2024-11-05 08:17:57 +0545

    adding

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('product_desc/<pk>',views.product_desc,name="product_desc"),
     path('add_to_cart/<pk>', views.add_to_cart,name="add_to_cart"),
     path('orderlist',views.orderlist,name="orderlist"),
+    path('add_item/<int:pk>',views.add_item,name="add_item"),
+    path('remove_item/<int:pk>',views.remove_item,name="remove_item"),
 ]
 
